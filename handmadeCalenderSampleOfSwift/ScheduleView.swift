87e408547dddc9efdce50a9329abdfed5ab5b18c
Morioka Naoya <g1989n@gmail.com>
2015-07-29 02:59:16 +0900

    とりあえず

---
+++ handmadeCalenderSampleOfSwift/ScheduleView.swift
@@ -0,0 +1,68 @@
+//
+//  ScheduleView.swift
+//  handmadeCalenderSampleOfSwift
+//
+//  Created by Morioka Naoya on H27/07/29.
+//  Copyright (c) 平成27年 just1factory. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    // Tableで使用する配列を設定する
+    var myItems: NSArray = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Viewのタイトルを設定
+        self.title = "Calendar Events"
+        
+        // Status Barの高さを取得する
+        let barHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.size.height
+        
+        // Viewの高さと幅を取得する
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        // TableViewの生成する（status barの高さ分ずらして表示）
+        let myTableView: UITableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        
+        // Cell名の登録を行う
+        myTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        // DateSourceの設定をする
+        myTableView.dataSource = self
+        
+        // Delegateを設定する
+        myTableView.delegate = self
+        
+        // Viewに追加する
+        self.view.addSubview(myTableView)
+        
+        for x in myItems {
+            println(x)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    /** 以下、tableview系メソッド **/
+    
+    /**
+    Cellが選択された際に呼び出される
+    **/
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath){
+        println("Num: \(indexPath.row)")
+        println("Value: \(myItems[indexPath.row])")
+    }
+    
+
+
+}
+
+
