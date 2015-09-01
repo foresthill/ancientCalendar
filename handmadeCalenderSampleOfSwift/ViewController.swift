7d97905826cc821d4696623aefbdb1a0dbdecf3e
Morioka Naoya <g1989n@gmail.com>
2015-09-02 05:59:31 +0900

    Could not find a storyboard named 'NextView' in bundle NSBundle

--- handmadeCalenderSampleOfSwift/ViewController.swift
+++ handmadeCalenderSampleOfSwift/ViewController.swift
@@ -342,7 +342,7 @@ class ViewController: UIViewController {
     func setupCalendarTitleLabel() {
         //calendarBar.text = String("\(year)年\(month)月のカレンダー")
         var jpnMonth = ["睦月", "如月", "弥生", "卯月", "皐月", "水無月", "文月", "葉月", "長月", "神無月", "霜月", "師走"]
-        calendarBar.text = String(jpnMonth[month-1] + "（旧暦\(month)月）")
+        calendarBar.text = String("" + jpnMonth[month-1] + "（旧暦\(month)月）")
     }
     
     //現在（初期表示時）の年月に該当するデータを取得する関数
@@ -473,58 +473,6 @@ class ViewController: UIViewController {
         toScheduleView()
     }
     
-    // Windowを開くアクション
-/*    internal func openWindow(button: UIButton){
-        
-        popUpWindow.backgroundColor = UIColor.whiteColor()
-        popUpWindow.frame = CGRectMake(0, 0, 200, 250)
-        popUpWindow.layer.position = CGPointMake(self.view.frame.width/2, self.view.frame.height/2)
-        popUpWindow.alpha = 0.8
-        popUpWindow.layer.cornerRadius = 10
-        
-        // popUpWindowsをkeyWindowsにする.
-        popUpWindow.makeKeyWindow()
-        
-        // windowsを表示する
-        self.popUpWindow.makeKeyAndVisible()
-        
-        // ボタンを作成する
-        popUpWindowButton.frame = CGRectMake(0, 0, 100, 60)
-        popUpWindowButton.backgroundColor = UIColor.orangeColor()
-        popUpWindowButton.setTitle("Close", forState: .Normal)
-        popUpWindowButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
-        popUpWindowButton.layer.masksToBounds = true
-        popUpWindowButton.layer.cornerRadius = 10.0
-        popUpWindowButton.layer.position = CGPointMake(self.popUpWindow.frame.width/2, self.popUpWindow.frame.height-50)
-        popUpWindowButton.addTarget(self, action: "onClickCloseButton:", forControlEvents: .TouchUpInside)
-
-        self.popUpWindow.addSubview(popUpWindowButton)
-        
-        // TextViewを作成する
-        let windowTextView: UITextView = UITextView(frame: CGRectMake(10, 10, self.popUpWindow.frame.width - 20, 150))
-        windowTextView.backgroundColor = UIColor.clearColor()
-        windowTextView.text = "ボタンを押しましたね。あなたは今日から“大納言”です。ウソです。"
-        windowTextView.text = "\(year)年\(month)月\(button.tag)日が選択されました！ by 大納言小豆"
-        windowTextView.font = UIFont.systemFontOfSize(CGFloat(15))
-        windowTextView.textColor = UIColor.blackColor()
-        windowTextView.textAlignment = NSTextAlignment.Left
-        windowTextView.editable = false
-        
-        self.popUpWindow.addSubview(windowTextView)
-        
-    }
-
-    
-    // Windowを閉じるイベント
-    internal func onClickCloseButton(sendar: UIButton){
-    
-        objc_setAssociatedObject(UIApplication.sharedApplication(), &popUpWindow, nil, UInt(OBJC_ASSOCIATION_RETAIN_NONATOMIC))
-        popUpWindow = nil
-        
-        // メインウインドウをキーウインドウにする。
-        UIApplication.sharedApplication().windows.first?.makeKeyAndVisible()
-    }
-  */
     // スケジュール画面に遷移
     internal func toSchedule(button: UIButton){
         // 遷移するViewを定義する
@@ -534,8 +482,9 @@ class ViewController: UIViewController {
         mySecondViewController.modalTransitionStyle = UIModalTransitionStyle.PartialCurl
         
         // Viewの移動する
-        self.presentViewController(mySecondViewController, animated: true, completion: nil)
-    }
+        //self.presentViewController(mySecondViewController, animated: true, completion: nil)
+        
+     }
     
     /**
     認証ステータスを取得
@@ -660,8 +609,12 @@ class ViewController: UIViewController {
             }
         }
         
+        var nex = UIStoryboard(name: "NextView", bundle: nil).instantiateViewControllerWithIdentifier("next") as! UIViewController
+        self.presentViewController(nex, animated: true, completion: nil);
+
+        
         // 画面遷移.
-        moveViewController(eventItems)
+        //moveViewController(eventItems)
         
     }
     
