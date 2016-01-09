5c65307c19521ffff34ec1800481fe3d39274295
Morioka Naoya <g1989n@gmail.com>
2016-01-09 10:25:00 +0900

    時間を「UIButton」にし、タップすると、DatePickerを呼び出す？→TextFieldで対応可能みたい。とりあえず断面保存。

--- handmadeCalenderSampleOfSwift/CalendarChangeViewController.swift
+++ handmadeCalenderSampleOfSwift/CalendarChangeViewController.swift
@@ -10,18 +10,25 @@ import Foundation
 import UIKit
 import EventKit
 
-class CalendarChangeViewController: UIViewController {
+class CalendarChangeViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var eventTitle: UILabel!
     //@IBOutlet weak var scheduleTitle: UILabel!
-    
-    @IBOutlet weak var startTime: UILabel!
-    @IBOutlet weak var endTime: UILabel!
-    @IBOutlet weak var location: UILabel!
+//    @IBOutlet weak var eventTitle: UILabel!
+//    @IBOutlet weak var startTime: UILabel!
+//    @IBOutlet weak var endTime: UILabel!
+//    @IBOutlet weak var location: UILabel!
     @IBOutlet weak var detailText: UITextView!
     @IBOutlet weak var updateButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
 
+    @IBOutlet weak var eventTitle: UITextField!
+    @IBOutlet weak var startTime: UITextField!
+    @IBOutlet weak var endTime: UITextField!
+    @IBOutlet weak var location: UITextField!
+
+//    @IBOutlet weak var startTime: UIButton!
+//    @IBOutlet weak var endTime: UIButton!
+    
     //前画面から取ってきたイベント
     var myEvent: EKEvent!
     
@@ -31,15 +38,49 @@ class CalendarChangeViewController: UIViewController {
     
     override func viewDidLoad() {
         eventTitle.text = myEvent.title
-        startTime.text = "\(myEvent.startDate)"
-        endTime.text = "\(myEvent.endDate)"
+        startTime.setTitle("\(myEvent.startDate)", forState: .Normal)
+        endTime.setTitle("\(myEvent.endDate)", forState: .Normal)
         location.text = myEvent.location
         detailText.text = myEvent.description
-        
+
     }
     
+    /*
+    TextFieldの文字が変わった時に呼ばれるメソッド
+    */
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        print("textFieldが変わったよ。")
+        myEvent.title = eventTitle.text!
+//        myEvent.startDate = startTime.titleLabel
+//        myEvent.endDate = endTime.text
+        myEvent.location = location.text
+//        myEvent.description = detailText.text
+        
+        return true
+    }
+    
+    /*
+    func FormatFromStringToDate(str:String){
+        let df = NSDateFormatter()
+        df.dateFormat = "yyyy/mm/dd hh:mm"
+        //let mySelectDateString = df.stringFromDate(<#T##date: NSDate##NSDate#>)
+        
+    }
+
+    func FormatFromDateToString(date:NSDate){
+        let df = NSDateFormatter()
+        df.dateFormat = "yyyy/mm/dd hh:mm"
+        let mySelectDateString = df.stringFromDate(date)
+        let mySelectDate = df.dateFromString(mySelectDateString)!
+        //myDate = NSDate(timeInterval: 0, sinceDate: mySelectDate)
+        
+    }
+    */
+
     @IBAction func updateAction(sender: UIButton){
         print("setCalendar")
+        
+        myEventStore = EKEventStore()
         
         //イベントを登録する
         myEvent.calendar = myEventStore.defaultCalendarForNewEvents
@@ -88,6 +129,42 @@ class CalendarChangeViewController: UIViewController {
         //変数を渡す
         //svc.myItems = eventItems;
         cdvc.myEvent = myEvent
+    }
+    
+    /*
+    認証許可
+    */
+    func allowAuthorization() {
+        print("allowAuthorized")
+        
+        //ステータスを取得
+        let status: EKAuthorizationStatus = EKEventStore.authorizationStatusForEntityType(EKEntityType.Event)
+        
+        if status != EKAuthorizationStatus.Authorized {
+            //ユーザに許可を求める
+            myEventStore.requestAccessToEntityType(EKEntityType.Event, completion: {(granted, error) -> Void in
+                
+                //許可が得られなかった場合アラート発動
+                if granted {
+                    return
+                }
+                else {
+                    
+                    //メインスレッド 画面制御 非同期
+                    dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                        
+                        //アラート作成
+                        let myAlert = UIAlertController(title: "許可されませんでした", message: "Privacy->App->Reminderで変更してください", preferredStyle: UIAlertControllerStyle.Alert)//UIAlertActionStyle→UIAlertControllerStyle.Alert
+                        
+                        //アラートアクション
+                        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+                        
+                        myAlert.addAction(okAction)
+                        self.presentViewController(myAlert, animated: true, completion: nil)
+                    })
+                }
+            })
+        }
     }
     
     // どのクラスにもあるメソッド Memory監視？
